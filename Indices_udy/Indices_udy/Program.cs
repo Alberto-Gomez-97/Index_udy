d601a3bc8842af155f56658bffc3e5466a27c75c
Alberto-Gomez-97 <120439839+Alberto-Gomez-97@users.noreply.github.com>
2023-08-16 16:50:03 -0600

    Add files via upload

---
+++ Indices_udy/Indices_udy/Program.cs
@@ -0,0 +1,14 @@
+﻿//INDICES
+//
+
+var VOCALES = new char[] {'a','e','i','o','u'};
+
+var segundaVocal = VOCALES[1];
+ var ultimaVocal = VOCALES[^1]; //para acceder al último lugar del arreglo
+var penultimaVocal = VOCALES[^2];//o //para acceder al penlúltimo lugar del arreglo
+
+Index penultimo = ^2;//para acceder al penlúltimo lugar del arreglo
+
+var penultimoVocal = VOCALES[penultimo];
+
+Console.WriteLine(penultimaVocal);//o
